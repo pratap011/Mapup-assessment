7a0727e137555a2ae5a2bd1d7d524bcb3391ac22
pratap011 <74375089+pratap011@users.noreply.github.com>
2023-07-27 22:43:45 +0530

    Added footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PlainFooter = () => {
   return (
     <footer style={{ backgroundColor: 'white', padding: '20px', textAlign: 'center', width:"100%",top:"100%",position:"absolute",height:"20%"}}>
-            <h2>Footer Section</h2>
+    <h2>Footer Section</h2>
     </footer>
   );
 };
