77967f7dc55f36c65dbcac89d1e929c56fd36554
pratap011 <74375089+pratap011@users.noreply.github.com>
2023-07-28 12:01:55 +0530

    Added Load button

--- src/formInput.js
+++ src/formInput.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Dropdown, message, Space, Tooltip } from 'antd';
 import {changeLocation} from './redux/location'
 import {useDispatch,useSelector} from 'react-redux'
@@ -7,13 +7,18 @@ const FormInput = () => {
     //The useDispath hook is used here to alter the state of the redux toolkit store based on the user choice and input.
 
     const dispatch=useDispatch();
+    const [option,setOption]=useState('Select Country')
 
 
     const handlemenuclick= (e) => {
         //This function takes in the user input and changes the state in redux to that specific country.
-
-        dispatch(changeLocation(e.key));
+        setOption(e.key);
       };
+    
+      const handleSubmitClick = ()=>{
+          dispatch(changeLocation(option));
+      }
+
 
     //the options available for the users with it's key.
     const items= [
@@ -35,14 +40,15 @@ const FormInput = () => {
         items,
         onClick:handlemenuclick
     }
-    
+
   return (
-    <div>
-        <Space wrap>
-    <Dropdown.Button style={{width:20}}size='large' menu={menuProps}>
-      Select Country
+    <div style={{display:"flex", justifyContent:"space-evenly",width:"40%"}}>
+    <div className='inputsection'>
+    <Dropdown.Button style={{width:20}}size='medium' menu={menuProps}>
+      {option}
     </Dropdown.Button>
-    </Space>
+    </div>
+    <Button type="primary" size='medium' onClick={handleSubmitClick}>Load</Button>
     </div>
 
     
